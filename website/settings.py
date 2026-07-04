3824fbacd6634b1308d917df4a2852e35b3839b0
ramyathangaraj094 <ramyathangaraj094@gmail.com>
2026-07-04 08:17:05 +0530

    Remove leading slash from STATIC_URL

--- website/settings.py
+++ website/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATICFILES_DIRS=[ BASE_DIR / "static",
                   ]
