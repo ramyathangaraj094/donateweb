464cab947fcb4b29576867a5595d57614643dc54
ramyathangaraj094 <ramyathangaraj094@gmail.com>
2026-07-04 08:10:26 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -118,10 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR), 'static']
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-MEDIA_URL = "/images/"
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS=[ BASE_DIR / "static",
+                  ]
+STATIC_ROOT =BASE_DIR / "staticfiles"
+
 
 
 
